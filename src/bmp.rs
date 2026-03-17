1e7093963a34cb7c5b5665fb41a7bf65a3e44a1d
shob <shobsbot@gmail.com>
2026-03-17 18:25:12 -0500

    working on reading pixels from bytes

--- src/bmp.rs
+++ src/bmp.rs
@@ -41,12 +41,24 @@ pub struct BmpImage {
 }
 
 impl BmpImage {
-    pub fn new(dib_header: BitMapImageHeader, file: &str) -> Result<BmpImage, ImageError> {
+    pub fn new(dib_header: BitMapImageHeader, mut file: File) -> Result<BmpImage, ImageError> {
         let mut buffer = Vec::new();
-        let file = File::open(file)?;
         file.read(&mut buffer)?;
         let image = &buffer[dib_header.size as usize..];
-        let pixels = image;
+        let row_size = (((dib_header.bits_per_pixel as i32 * dib_header.width) / 32) * 4) as usize;
+        // let array_size = row_size * dib_header.height.abs();
+        let padding = (dib_header.width * 4 - dib_header.width * 3) as usize;
+        let mut pixels = Vec::new();
+        for i in 0..dib_header.height.abs() as usize {
+            let mut pixel_row = Vec::new();
+            for j in 0..((row_size - padding) / 3) {
+                let blue = image[(i * row_size) + j * 3];
+                let green = image[(i * row_size) + (j + 1) * 3];
+                let red = image[(i * row_size) + (j + 2) * 3];
+                pixel_row.push(Rgb { red, green, blue });
+            }
+            pixels.push(pixel_row);
+        }
         Ok(BmpImage {
             dib_header: dib_header,
             pixels: pixels,
