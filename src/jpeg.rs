555ed4392d2a2bd083515cab08538ac736d0f093
shob <shobsbot@gmail.com>
2026-05-05 09:46:39 -0500

    started segment reading for jpeg

--- src/jpeg.rs
+++ src/jpeg.rs
@@ -2,17 +2,30 @@ use crate::error::ImageError;
 
 #[derive(Debug)]
 struct JpegChunk {
-    indicater: u8,
-    length: Option<u16>,
+    indicator: u8,
+    length: u16,
     data: Vec<u8>,
 }
 
 impl JpegChunk {
     fn new(bytes: &[u8]) -> Result<JpegChunk, ImageError> {
         let indicator: u8 = bytes[1];
-        let new_chunk = JpegChunk {
-            length: Some(length),
-        }
+        let length = match indicator {
+            0xD8 | 0xD9 => 0 as u16,
+            0xC0 | 0xC2 | 0xC4 | 0xDB | 0xDA | 0xFE => u16::from_be_bytes(bytes[2..4].try_into()?),
+            0xDD => 4 as u16,
+            _ => {
+                return Err(ImageError::CustomError(format!(
+                    "invalid indicator: {:#X}",
+                    indicator
+                )));
+            }
+        };
+        Ok(JpegChunk {
+            indicator: indicator,
+            length: length,
+            data: bytes[4..4 + length as usize].to_vec(),
+        })
     }
 }
 
